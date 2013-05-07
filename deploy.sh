8e6dcbb7825d040d85b92a655e288af6cf9b438d
huubby <huubby1@gmail.com>
2013-05-07 13:22:13 +0800

    Add gitconfig

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ fi
 
 
 OLDDIR=~/dotfiles_old
-DOTFILES="bashrc vimrc vim"
+DOTFILES="bashrc vimrc vim svn.bashrc gitconfig"
 
 echo "Creating $OLDDIR for backup of any existing dotfiles in ~"
 mkdir -p $OLDDIR
