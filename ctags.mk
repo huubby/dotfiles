f0179e08efeac7c1c45016e5a1349160cb5a3fce
huubby <huubby1@gmail.com>
2014-01-09 14:53:38 +0800

    Replace ctags.mk and cscope.mk with single script.

--- ctags.mk
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -f tags --exclude=*_bak --exclude=*python*
--- updatetags.sh
+++ updatetags.sh
@@ -1,3 +1,7 @@
-#! /bin/sh
+#!/bin/bash
+
+cd $1
 find . -type f -regex ".*\.cpp" -or -regex ".*\.c" -or -regex ".*\.hpp" -or -regex ".*\.inl"|grep -v _bak|grep -v python > cscope.files
 cscope -bq
+ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -f tags --exclude=*_bak --exclude=*python*
+rm -rf cscope.files
