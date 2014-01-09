5a15d8be5c38d299277f5a1d51e6050e2788498b
huubby <huubby1@gmail.com>
2014-01-09 14:14:35 +0800

    Hg configurations.

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ fi
 
 
 OLDDIR=~/dotfiles_old
-DOTFILES="bashrc vimrc vim svn.bashrc gitconfig tmux.conf"
+DOTFILES="bashrc vimrc vim svn.bashrc gitconfig tmux.conf hgrc"
 
 echo "Creating $OLDDIR for backup of any existing dotfiles in ~"
 mkdir -p $OLDDIR
